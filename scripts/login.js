af6504e454a332ef06cc99ad11f0212c077795e5
Giovanni Benner <giovanni.guilioti@benner.com.br>
2022-10-17 11:16:27 -0300

    login validation

---
+++ scripts/login.js
@@ -0,0 +1,43 @@
+function ValidarLogin()
+{
+    fetch('usuario.json')
+    .then((response) => response.json())
+    .then((data) => {
+        // console.log(data["users"])
+
+        let username = document.getElementById("user").value;
+        let password = document.getElementById("password").value;
+
+        let validate = false;
+        data["users"].forEach(user => {
+            if(user.user == username)
+            {
+                if(user.pws == password)
+                {
+                    validate = true;
+                    return;
+                }
+            }
+        }
+        );
+        
+        try
+        {
+            if(validate)
+            {
+                let form = document.getElementById('login');
+                form.action = "painel.html";
+                form.submit();
+            }
+            else
+            {
+                throw "Credenciais Erradas";
+            }
+        }catch(erro)
+        {
+            alert(erro);
+        }
+    })
+
+}
+
